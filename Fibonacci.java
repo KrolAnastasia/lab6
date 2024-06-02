2cd48db45e12407731b9e30e89e8116267d153ab
KrolAnastasia <crolnastya@bk.ru>
2024-06-02 20:04:07 +0300

    8: changed variable name

--- Fibonacci.java
+++ Fibonacci.java
@@ -44,7 +44,7 @@ public class Main {
     public static double integration(Function<Double, Double> f, double a, double b) {
         double step = (b - a) / 100000;
         double aVal = f.apply(a);
-        double bVal = f.apply(b);
+        double bValue = f.apply(b);
         double funcSum = 0;
         for (double i = a + step; i < b; i += step) {
             funcSum += f.apply(i);
