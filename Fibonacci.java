daf616c991b839461e51e3509ac8d35b3ed69a07
KrolAnastasia <crolnastya@bk.ru>
2024-06-02 19:59:25 +0300

    3: changed return(conflict with 7 in second-branch)

--- Fibonacci.java
+++ Fibonacci.java
@@ -21,7 +21,7 @@ public class Main {
     }
 
     public static long factorial(long n) {
-        if (n == 0) return 1;
+        if (n == 0) return 0;
         return n * factorial(n - 1);
     }
 
